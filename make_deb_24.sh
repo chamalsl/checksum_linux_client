3f09d90f545ffeea026d363490ac31964ff5016a
chamalsl <1069808+chamalsl@users.noreply.github.com>
2025-04-24 07:47:28 +0530

    Added libsecret to dependancies in make_deb_24.sh.

--- make_deb_24.sh
+++ make_deb_24.sh
@@ -6,14 +6,14 @@ mkdir -p ${RELEASE_DIR}/usr/local/bin
 mkdir -p ${RELEASE_DIR}/DEBIAN
 touch ${RELEASE_DIR}/DEBIAN/control
 cp build/release/checksums release/Ubuntu24/
-cp build/release/checksums ${RELEASE_DIR}/usr/local/bin/is-online
+cp build/release/checksums ${RELEASE_DIR}/usr/local/bin/checksums
 
 echo "Package: checksums" > ${RELEASE_DIR}/DEBIAN/control
 echo "Version: ${VERSION}" >> ${RELEASE_DIR}/DEBIAN/control
 echo "Architecture: amd64" >> ${RELEASE_DIR}/DEBIAN/control
 echo "Maintainer: Chamal De Silva <chamaldesilva@gmail.com>" >> ${RELEASE_DIR}/DEBIAN/control
 echo "Description: Program which verifies checksum of a file." >> ${RELEASE_DIR}/DEBIAN/control
-echo "Depends: libgtkmm-3.0-1t64 (>= 3.24.9), libcurl4t64 (>= 7.16.2) , libsqlite3-0 (>= 3.5.9)" >> ${RELEASE_DIR}/DEBIAN/control
+echo "Depends: libgtkmm-3.0-1t64 (>= 3.24.9), libcurl4t64 (>= 7.16.2) , libsecret-1-0 (>= 0.21.4-1build3)" >> ${RELEASE_DIR}/DEBIAN/control
 cd release/Ubuntu24/
 dpkg-deb --build --root-owner-group ${RELEASE_FILE}/
 

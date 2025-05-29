28df0e01363dfd3ee7710752af2a4504803c1184
chamalsl <1069808+chamalsl@users.noreply.github.com>
2025-05-29 19:04:11 +0530

    Added support for checking sha512.

--- api.h
+++ api.h
@@ -2,20 +2,21 @@
 #define API_H
 
 #include <string>
+#include "result.h"
 #include "third_party/json_parser/json_parser.h"
 
 static const std::string URL_API_GET_TOKEN = "https://checksums.rammini.com/tokens/gettoken";
 static const std::string URL_API_FIND_BY_NAME = "https://checksums.rammini.com/api/findByFileName";
 static const std::string URL_API_FIND_BY_NAME_AUTH = "https://checksums.rammini.com/api/findByFileNamePrivate";
-static const std::string URL_API_FIND_BY_SHA = "https://checksums.rammini.com/api/findBySha256";
-static const std::string URL_API_FIND_BY_SHA_AUTH = "https://checksums.rammini.com/api/findBySha256Private";
+static const std::string URL_API_FIND_BY_CHECKSUMS = "https://checksums.rammini.com/api/findByChecksums";
+static const std::string URL_API_FIND_BY_CHECKSUMS_AUTH = "https://checksums.rammini.com/api/findByChecksumsPrivate";
 
 class Api{
 
     public:
         static std::pair<short, std::string> findByFileName(std::string file_name, std::string apiToken);
-        static std::pair<short, std::string>  findBySha256(std::string sha256, std::string apiToken);
-        static std::string getResultToDisplay(JsonObject *file_json, std::string local_file_sha256);
+        static std::pair<short, std::string>  findByChecksums(std::string sha256, std::string sha512, std::string apiToken);
+        static std::string getResultToDisplay(JsonObject *file_json, std::string local_file_sha256, std::string local_file_sha512, Result::RESULT_TYPE &result_type);
 };
 
 #endif //API_H
